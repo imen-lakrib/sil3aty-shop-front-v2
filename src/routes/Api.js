d547233e6b9adcf6cdf9306ef4b410046597347c
imen-lakrib <lak12imen@gmail.com>
2023-07-27 11:30:46 +0100

    test a new uri in vercel

--- src/routes/Api.js
+++ src/routes/Api.js
@@ -1,6 +1,7 @@
 // const API_URL = process.env.REACT_APP_API_URL;
 // const API_URL = "http://localhost:3012/";
+const API_URL = "https://sil3aty-shop-backend.vercel.app/";
 
 
-const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = process.env.REACT_APP_API_URL;
 export default API_URL;
