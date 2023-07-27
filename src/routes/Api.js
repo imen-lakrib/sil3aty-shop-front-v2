e1fcc36f479ff602f264933e3b94dfb32700014e
imen-lakrib <lak12imen@gmail.com>
2023-07-27 20:01:00 +0100

    conect with vercel uri

--- src/routes/Api.js
+++ src/routes/Api.js
@@ -1,6 +1,6 @@
 // const API_URL = process.env.REACT_APP_API_URL;
-const API_URL = "http://localhost:3012/";
-// const API_URL = "https://sil3aty-shop-backend.vercel.app/";
+// const API_URL = "http://localhost:3012/";
+const API_URL = "https://sil3aty-shop-backend.vercel.app/";
 
 
 // const API_URL = process.env.REACT_APP_API_URL;
